f6665fa247694bc30e04d5a7b9bcc72c5a80689f
Arnau478 <arnauxabia@gmail.com>
2023-08-22 20:42:29 +0200

    Added module support

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
 
     const optimize = b.standardOptimizeOption(.{});
@@ -14,6 +14,12 @@ pub fn build(b: *std.Build) void {
     lib.linkLibC();
 
     b.installArtifact(lib);
+
+    const module = b.createModule(.{
+        .source_file = .{ .path = "src/main.zig" },
+    });
+
+    try b.modules.put(b.dupe("wasm"), module);
 
     var main_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
